ac48a9bc6bd0711f4b7676325022ec396ed8810c
Sugesh Chandran <sugesh.chandran@intel.com>
2017-03-17 23:03:31 +0000

    Add command execution function,
    Signed-off-by: Sugesh Chandran <sugesh.chandran@intel.com>

--- ovs_cli.py
+++ ovs_cli.py
@@ -7,9 +7,13 @@ import sys, tty, termios
 from ovs_cmd_dic import *
 import platform
 import argparse
+import subprocess
+import os
 
 # The global stack for context handling.
 gbl_token_stack = []
+cmd_input = ""
+cur_dic = [ovs_cmd]
 
 def getch():
     fd = sys.stdin.fileno()
@@ -24,6 +28,7 @@ def getch():
     return ch
 
 def push_tokenlist(token_list):
+    global gbl_token_stack
     if token_list:
         gbl_token_stack.append(token_list)
         return True
@@ -31,6 +36,7 @@ def push_tokenlist(token_list):
 
 # Pop the tokenlist from stack, return the default value id stack is empty
 def pop_tokenlist(default_tokenlist):
+    global gbl_token_stack
     if not gbl_token_stack:
         return default_tokenlist
     return gbl_token_stack.pop()
@@ -103,20 +109,51 @@ def process_tokensublist(cmd_input, token_diclist):
             token_strlist = token_strlist + tkn_list
     return token_strlist
 
+def clean_cli():
+    global gbl_token_stack
+    global cmd_input
+    global cur_dic
+    gbl_token_stack = []
+    cmd_input = ""
+    cur_dic = [ovs_cmd]
+
+def do_execute_cmd(cmd):
+    global OVS_SUDO_CMD
+    global OVS_BIN_PATH
+
+    if not cmd:
+        return True
+    exec_cmd = []
+    exec_cmd.append(cmd)
+    if OVS_SUDO_CMD:
+        #Need to run command in sudo mode.
+        ovs_sudo = OVS_SUDO_CMD.split()
+    exec_cmd = ovs_sudo + exec_cmd
+
+    exec_cmd = filter(None, exec_cmd)
+    if OVS_BIN_PATH:
+        cmd_env = os.environ.copy()
+        cmd_env["PATH"] = OVS_BIN_PATH + ":" + cmd_env["PATH"]
+    try:
+        out = subprocess.Popen(exec_cmd, env = cmd_env)
+    except Exception as e:
+        raise e
+    out.wait()
+
 def parse_pgm_args():
     global OVS_BIN_PATH
     global OVS_SUDO_CMD
 
     parser = argparse.ArgumentParser(description="ovs-cli    :"
                                      "    CLI for OVS Debugging")
-    parser.add_argument('-p','--path', help='Path to OVS binary files, seperated by :',
+    parser.add_argument('-p','--path', help='Absolute path to OVS binary files, seperated by :',
                         type=str, default="",dest="path",required=False)
     parser.add_argument('-s','--sudo', help='Execute commands in sudo',
                         action="store_true", dest="sudo_run", default=False,
                         required=False)
     args = parser.parse_args()
     if args.path:
-        OVS_BIN_PATH = OVS_BIN_PATH + args.path
+        OVS_BIN_PATH = args.path
     if args.sudo_run:
         OVS_SUDO_CMD = "sudo -E"
 
@@ -126,10 +163,6 @@ if __name__ == '__main__':
                                      "platform for now")
         exit(1)
     global OVS_CLI_CMD_PROMPT
-    cmd_input = ""
-    mask = ""
-    cur_dic = [ovs_cmd]
-    cur_elem =None
 
     parse_pgm_args()
     print_banner()
@@ -164,7 +197,13 @@ if __name__ == '__main__':
                 print_mask(OVS_CLI_CMD_PROMPT + cmd_input)
             continue
         elif ch_byte == 0xD: # New line
-            exit()
+            try:
+                do_execute_cmd(cmd_input)
+            except:
+                print("\nInvalid command..\n")
+            else:
+                clean_cli()
+            continue
         elif ch_byte < 0x20 or ch_byte > 0x7E: # Special characters
             exit()
 
